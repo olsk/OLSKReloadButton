2c298691a3480259e422aee1e447cb3f56f9a60c
Rosano Coutinho <pub@xeari.com>
2019-09-16 17:34:04 -0400

    Use declared OLSKLocalized in stub

--- stub-view.svelte
+++ stub-view.svelte
@@ -1,7 +1,11 @@
 <script>
 import Module from './main.svelte';
 
-import { OLSKLocalized } from '../common/global.js'
+let localizationDictionary = JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`);
+import OLSKInternational from 'OLSKInternational';
+export const OLSKLocalized = function(translationConstant) {
+	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, localizationDictionary[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
+};
 </script>
 
 <Module OLSKLocalized={ OLSKLocalized } />
