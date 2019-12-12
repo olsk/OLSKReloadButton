a5c68ab3b2a2026f788616e3dd52e562aaab3f92
Rosano Coutinho <pub@xeari.com>
2019-12-12 18:24:54 -0500

    Fix typo

--- stub-view.svelte
+++ stub-view.svelte
@@ -3,7 +3,7 @@ import Module from './main.svelte';
 
 let localizationDictionary = JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`);
 import OLSKInternational from 'OLSKInternational';
-export const OLSKLocalized = function(translationConstant) {
+const OLSKLocalized = function(translationConstant) {
 	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, localizationDictionary[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
 };
 </script>
