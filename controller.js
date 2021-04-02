e6f41d6d04ea69152129dfad23324fd4dc785dba
Rosano Coutinho <pub@xeari.com>
2021-04-02 12:32:52 -0300

    Localize pt

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRouteFunction (req, res, next) {
 				return res.render(require('path').join(__dirname, 'stub-view'));
 			},
-			OLSKRouteLanguageCodes: ['en', 'fr', 'es'],
+			OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt'],
 		},
 	];
 };
